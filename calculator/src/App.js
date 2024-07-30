7b8835c857d7584ec11f6722c02853e0da4aa9e2
Khushboo <patelkhushboo057@gmail.com>
2024-07-30 23:14:18 +0530

    initial commit on the calculator

---
+++ calculator/src/App.js
@@ -0,0 +1,13 @@
+import Calculator from './Calculator';
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Calculator/>
+    </div>
+  );
+}
+
+export default App;
